3205f48bb3abd1385bed6fbbd490ca3d18cab541
Jui <pathanin.pra@dome.tu.ac.th>
2024-10-18 19:38:46 +0700

    Call TU api and confirm that userinputs are match api data

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static("public"));
 const app = express();
 const port = 3000;
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req,res) => {
     res.sendFile(__dirname + "/index.html");
 })
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
