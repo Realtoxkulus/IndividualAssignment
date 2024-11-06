5adf60c2f0f21158cae59fac653060aa2d2723d3
Jui <pathanin.pra@dome.tu.ac.th>
2024-11-06 15:28:04 +0700

    Replace all files with new files

--- server.js
+++ server.js
@@ -1,20 +1,52 @@
-import express from "express";
+import axios from "axios";
 import bodyParser from "body-parser";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
+import express, { response } from "express";
+import ejs from "ejs"
 
-const app = express();
 const port = 3000;
+const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", (req,res) => {
-    res.sendFile(__dirname + "/index.html");
+const apiUrl = "https://restapi.tu.ac.th/api/v1/auth/Ad/verify";
+const apiToken = "TUa102b22222c092b0a4d8880665add4bfc4cd74b48ef7b7e2d657e126b6faaf63148ead8e7ce823d2587ec03686d3da84"
+const config = {
+    headers: {
+        "Content-Type": "application/json",
+        "Application-Key": `${apiToken}`
+    },
+};
+
+app.get("/" , (req,res) => {
+    res.render("index.ejs")
 })
 
+app.post("/" , async (req,res) => {
+    const data = {
+        UserName: req.body.username,
+        PassWord: req.body.password,
+    }
 
-app.listen(port, () => {
+    try{
+        const response = await axios.post(`${apiUrl}` , data , config)
+        const responseJS = response.data
+        console.log(responseJS)
+        res.render("index.ejs" , {
+            message: responseJS.message,
+        })
+    } catch (error) {
+        res.render("index.ejs" , {
+            error: error.response.data.message,
+        })
+        console.log(error.response.data)
+        res.status(500);
+        
+    }
+    
+    
+});
+
+app.listen(port , () => {
     console.log(`Server running on port ${port}`);
 })
